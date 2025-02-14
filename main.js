76f273a482acd39ad5422ec5b6dde764fb17103a
shaaav1e <shaaavie@gmail.com>
2025-02-14 16:11:47 +0500

    Minimal fixes are required

--- main.js
+++ main.js
@@ -6,3 +6,25 @@ navToggle.addEventListener("click", () =>{
   : navToggle.setAttribute("aria-expanded",true)
   primaryNav.toggleAttribute('data-visible');
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // pagination: {
+    //   el: '.swiper-pagination', //these enable dots
+    //   clickable: true,
+    // },
+    effect: 'slide',
+  });
+});
